d85696345e58233da659390600d18d493be59986
Carlitoz123 <111470132+Carlitoz123@users.noreply.github.com>
2025-10-21 12:58:19 -0600

    user controller y poquito mas

---
+++ empresa/app/Http/Controllers/UsersController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth');
+    }
+    public function getUsers(){
+        /** Select * from users */
+        $data= User::all();
+        //dd($data);
+        return view("admin.users")
+        ->with('usuarios',$data);
+    }
+    public function createUsers(Request $request){
+       // dd($request->email);
+       //reglas de validacion
+       $request->validate([
+        "name"=>'required|min:3',
+        "nickname"=>"required|min:3|unique:users,nickname",
+        "email"=>"required|email|unique:users,email",
+        "password"=>'required|min:4',
+        "password2"=>'required|min:4|same:password'
+       ]);
+       //guardar registro insert into user.......
+       $user = new User();
+       $user->name=$request->name;
+       $user->nickname=$request->nickname;
+       $user->password=Hash::make($request->password);
+       $user->email=$request->email;
+       $user->img= "default.jpg";
+       $user->save();
+       return redirect()
+            ->back()
+            ->with('success',"Usuario insertado correctamente");
+    }
+}
--- empresa/routes/web.php
+++ empresa/routes/web.php
@@ -1,10 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\DashboardController;
 
+//ENDPOINT
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+    Route::group(['prefix'=>'dashboard'],function(){
+
+    Route::resource('/',DashboardController::class);
+    
+
+    RoutE::get("/users",[UsersController::class,'getUsers']);
+    RoutE::post("/users",[UsersController::class,'createUsers']);
+
+
+});
+
+
 
 Auth::routes();
 
